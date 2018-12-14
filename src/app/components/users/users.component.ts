55a0d888197abc8f980ac45504f43bd510d4ace3
Jona Hyun <jona.hyun@gmail.com>
2018-12-14 16:46:41 -0500

    lesson on ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('test me');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+            street: '50 main st',
+            city: 'Boston',
+            state: 'MA'
+        }
+      },
+      {
+        firstName: 'Sarah',
+        lastName: 'Smith',
+        age: 30,
+        address: {
+            street: '2323 Pricneton',
+            city: 'Louisville',
+            state: 'KY'
+        }
+      },
+      {
+        firstName: 'Cisol',
+        lastName: 'Hogan',
+        age: 41,
+        address: {
+            street: '2323 School St',
+            city: 'Liberty',
+            state: 'OH'
+        }
+    }
+
+    ];
+    this.addUser({
+      firstName: 'Adam',
+      lastName: 'Koehler',
+      age: 41,
+      address: {
+          street: '2323 School St',
+          city: 'Liberty',
+          state: 'OH'
+      }
+
+    });
+
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
