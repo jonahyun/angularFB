6912f24c7c79aa8957756fe7f9f8c08d045cfb70
Jona Hyun <jona.hyun@gmail.com>
2018-12-18 16:26:24 -0500

    conditionals using example of content load, showExtended, etc

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,43 +8,55 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-            street: '50 main st',
-            city: 'Boston',
-            state: 'MA'
-        }
-      },
-      {
-        firstName: 'Sarah',
-        lastName: 'Smith',
-        age: 30,
-        address: {
-            street: '2323 Pricneton',
-            city: 'Louisville',
-            state: 'KY'
-        }
-      },
-      {
-        firstName: 'Cisol',
-        lastName: 'Hogan',
-        age: 41,
-        address: {
-            street: '2323 School St',
-            city: 'Liberty',
-            state: 'OH'
-        }
-    }
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+              street: '50 main st',
+              city: 'Boston',
+              state: 'MA'
+          }
+        },
+        {
+          firstName: 'Sarah',
+          lastName: 'Smith',
+          age: 30,
+          address: {
+              street: '2323 Pricneton',
+              city: 'Louisville',
+              state: 'KY'
+          }
+        },
+        {
+          firstName: 'Cisol',
+          lastName: 'Hogan',
+          age: 41,
+          address: {
+              street: '2323 School St',
+              city: 'Liberty',
+              state: 'OH'
+          }
+      }
+  
+      ];
 
-    ];
+      this.loaded = true;
+  
+
+    }, 2000);
+
+   
+    this.showExtended = false;
+
     this.addUser({
       firstName: 'Adam',
       lastName: 'Koehler',
